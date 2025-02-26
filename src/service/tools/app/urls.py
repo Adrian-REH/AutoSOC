22944ea808237d22c6c8d88069adf810160491c1
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-02-26 16:22:20 +0100

    added allow ip and delete session by ip

--- src/service/tools/app/urls.py
+++ src/service/tools/app/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path
 from payments.views import process_payment
 from items.views import list_items, decrease_stock
-from actions.views import block_ip, follow_ip, webdriver_alert, csp_alert, health_check
-from user_sessions.views import list_sessions, save_session
+from actions.views import block_ip, follow_ip, webdriver_alert, csp_alert, health_check,allow_ip
+from user_sessions.views import list_sessions, save_session, delete_session_by_ip
 
 urlpatterns = [
     path('api/items/list/', list_items),
@@ -14,6 +14,8 @@ urlpatterns = [
     path('api/actions/follow/ip/', follow_ip),
     path('api/actions/notify-webdriver-detection/', webdriver_alert),
     path('api/actions/execute/cspalert/', csp_alert),
+    path('api/actions/allow/ip/', allow_ip),
     path('api/user-sessions/list/', list_sessions),
     path('api/user-sessions/', save_session),
+    path('api/user-sessions/delete/ip/', delete_session_by_ip),
 ]
