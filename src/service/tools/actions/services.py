afc9bd23079baee8ac937a7ebba94bbbf6513f63
Adrian-REH <adrianherrera.r.e@gmail.com>
2025-02-26 16:22:02 +0100

    fix ip recognized in alert service

--- src/service/tools/actions/services.py
+++ src/service/tools/actions/services.py
@@ -3,31 +3,75 @@ from rest_framework.decorators import api_view
 from .handle_email import send_email
 from user_sessions.services import save_one_session
 import requests
+from django.conf import settings
+import os
+import pika
+import json
 
 def process_webdriver_alert_and_notify(request):
-	session_processed = save_one_session(request.data)
-	if session_processed is None:
-		return {"error": "Error processing session"}
+	ips = request.META.get('HTTP_X_FORWARDED_FOR', '').split(',') if request.META.get('HTTP_X_FORWARDED_FOR') else [request.META.get('REMOTE_ADDR')]
 
-	alert_count = session_processed.get("alert_count")
-	is_blocked = session_processed.get("is_blocked")
-	ip = request.data.get("ip")
-	data = request.data.copy()
-	data["security_level"] = "high"
-	data["alert_type"] = "WebDriver detection"
-	body = f"""
-	Se ha detectado un nuevo evento con la siguiente información:
+	# Procesar cada IP
+	for ip in ips:
+		session_processed = save_one_session(request.data, ip)
+		if session_processed is None:
+			return {"error": "Error processing session"}
+		
+		# Obtener los datos relevantes de la sesión procesada
+		alert_count = session_processed.get("alert_count")
+		session_data = session_processed.get("session_data", {})
+		is_blocked = session_data.get("is_blocked")
+		session_ip = session_data.get("ip")
 
-	📍 Dirección IP: {request.data.get('ip')}
-	🖥️ User-Agent: {request.data.get('user_agent','Desconocido')}
-	🌍 Idioma: {request.data.get('language','Desconocido')}
-	💻 Plataforma: {request.data.get('platform','Desconocido')}
-	📏 Resolución de pantalla: {request.data.get('screen', {}).get('height', 'N/A')} x {request.data.get('screen', {}).get('width', 'N/A')}
-	🕒 Timestamp: {request.data.get('timestamp', 'Desconocido')}
+		# Si es necesario trabajar con request.data, puedes hacerlo de la siguiente manera
+		data = request.data.copy()  # Si es necesario modificar 'data' sin afectar el original
 
-	🔒 Nivel de seguridad: high
-	⚠️ Tipo de alerta: Webdriver detection, N°: {alert_count}"""
-	subject = f"""AutoSoc - IP: {ip}, Alert: Webdriver detection, level: high """
-	if is_blocked:
-		subject = f"""AutoSoc - IP: {ip} ¡Is Blocked!, Alert: Webdriver detection, level: high """
-	send_email(body, subject)
+		data["security_level"] = "high"
+		data["alert_type"] = "WebDriver detection"
+		subject = f"""AutoSoc - IP: {ip}, Alert: N° - {alert_count} Webdriver detection, level: high """
+		if is_blocked:
+			subject = f"""AutoSoc - IP: {ip} ¡Is Blocked!, Alert: Webdriver detection, level: high """
+			print(subject)
+			for ip in ips:
+				rabbitmq_send_ip(ip.strip(), True)
+
+		body = f"""
+		Se ha detectado un nuevo evento con la siguiente información:
+
+		📍 Dirección IP: {request.data.get('ip')}
+		🖥️ User-Agent: {request.data.get('user_agent','Desconocido')}
+		🌍 Idioma: {request.data.get('language','Desconocido')}
+		💻 Plataforma: {request.data.get('platform','Desconocido')}
+		📏 Resolución de pantalla: {request.data.get('screen', {}).get('height', 'N/A')} x {request.data.get('screen', {}).get('width', 'N/A')}
+		🕒 Timestamp: {request.data.get('timestamp', 'Desconocido')}
+
+		🔒 Nivel de seguridad: high
+		⚠️ Tipo de alerta: Webdriver detection"""
+
+		send_email(body, subject)
+
+
+
+def enviar_ip_bloqueada(channel,ip):
+	message = {'ip': ip}
+
+	channel.basic_publish(exchange='',
+						routing_key='block_ip_queue',
+						body=json.dumps(message))
+
+	print(f"IP {ip} enviada para ser bloqueada.")
+
+def rabbitmq_send_ip(ip, is_blocked):
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.HOST_RABBITMQ))
+	channel = connection.channel()
+
+	channel.queue_declare(queue='handle_ip_queue')
+	message = {'is_blocked': is_blocked, 'ip': ip}
+
+	channel.basic_publish(exchange='',
+						routing_key='handle_ip_queue',
+						body=json.dumps(message))
+
+	print(f"IP {ip} enviada para ser bloqueada.")
+	connection.close()
+
--- src/service/tools/actions/views.py
+++ src/service/tools/actions/views.py
@@ -2,47 +2,60 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .handle_email import send_email
 from user_sessions.services import save_one_session
-from .services import process_webdriver_alert_and_notify
+from .services import process_webdriver_alert_and_notify, rabbitmq_send_ip
 import requests
 from rest_framework import status
 
 @api_view(['GET'])
 def health_check(request):
-    return Response({"message": f"ON"})
+	
+	return Response({"message": f"ON"})
 
 
 @api_view(['POST'])
 def block_ip(request):
-    ip = request.data.get('ip')
-    # Aquí iría la lógica para bloquear la IP
-    return Response({"message": f"IP {ip} bloqueada exitosamente"})
+	ip = request.data.get('ip')
+	try:
+		rabbitmq_send_ip(ip, True)
+	except Exception as e:
+		print(e)
+	return Response({"message": f"IP {ip} bloqueada exitosamente"})
+
+@api_view(['POST'])
+def allow_ip(request):
+	ip = request.data.get('ip')
+	try:
+		rabbitmq_send_ip(ip, False)
+	except Exception as e:
+		print(e)
+	return Response({"message": f"Allowed IP: {ip}"})
 
 @api_view(['POST'])
 def follow_ip(request):
-    ip = request.data.get('ip')
-    # Aquí iría la lógica para hacer seguimiento a la IP
-    return Response({"message": f"Siguiendo actividad de IP: {ip}"})
+	ip = request.data.get('ip')
+	# Aquí iría la lógica para hacer seguimiento a la IP
+	return Response({"message": f"Siguiendo actividad de IP: {ip}"})
 
 @api_view(['POST'])
 def webdriver_alert(request):
-    """     url_power_automate = ''
-    headers = {
-        'Content-Type': 'application/json',
-    }
-    response = requests.post(url, json=data, headers=headers) """
-    ip = request.data.get("ip")
-    timestamp = request.data.get("timestamp")
-    try:
-        process_webdriver_alert_and_notify(request)
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return Response({"message": "Error"}, status=status.HTTP_400_BAD_REQUEST)
-        
+	"""     url_power_automate = ''
+	headers = {
+		'Content-Type': 'application/json',
+	}
+	response = requests.post(url, json=data, headers=headers) """
+	ip = request.data.get("ip")
+	timestamp = request.data.get("timestamp")
+	
+	try:
+		process_webdriver_alert_and_notify(request)
+	except Exception as e:
+		print(f"Error: {str(e)}")
+		return Response({"message": "Error"}, status=status.HTTP_400_BAD_REQUEST)
 
-    return Response({"message": f"Se ejecuto PowerAction Alert Use WebDriver IP: {ip} Timestamp: {timestamp}"})
+	return Response({"message": f"Se ejecuto PowerAction Alert Use WebDriver IP: {ip} Timestamp: {timestamp}"})
 
 @api_view(['POST'])
 def csp_alert(request):
-    ip = request.data.get('ip')
-    # Aquí iría la lógica para hacer seguimiento a la IP
-    return Response({"message": f"Se ejecuto PowerAction Alert non-compliance csp IP: {ip}"})
+	ip = request.data.get('ip')
+	# Aquí iría la lógica para hacer seguimiento a la IP
+	return Response({"message": f"Se ejecuto PowerAction Alert non-compliance csp IP: {ip}"})
